c068040e1b84b0c81fabd3bbd6dfab5fa85075bd
pyoja <113084653+pyoja@users.noreply.github.com>
2023-06-28 16:21:16 +0900

    Deps

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = () => setKeyword(event.target.value);
   console.log("난 항상 실행중!");
   useEffect(() => {
-    console.log("CALL THE API...!");
+    console.log("1번만 실행");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("키워드 변화에만 실행");
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("카운터 변화 실행");
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me!</button>
     </div>
