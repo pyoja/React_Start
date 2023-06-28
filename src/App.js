53c8600b3d7ea481c564de093da4e332995ad9ef
pyoja <113084653+pyoja@users.noreply.github.com>
2023-06-28 15:41:03 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("난 항상 실행중!");
+  useEffect(() => {
+    console.log("CALL THE API...!");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
