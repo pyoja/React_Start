f91a926e82079b420bb2ffd9e20f09dd0bb2ce7e
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-03 16:13:16 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
+      <h1>The COIN TRADE {coins.length}</h1>
+      {loading ? <strong>Loading...</strong> : null}
       <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+          </li>
         ))}
       </ul>
     </div>
