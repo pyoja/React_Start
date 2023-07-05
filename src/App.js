031ff13213ba12d560d6cef36a1a143a8da97d13
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-05 11:01:54 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/hello" element={<h1>Hello!!!!</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
